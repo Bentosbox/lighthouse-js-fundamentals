7d142f543494ff51362f24a50593805b2b4d586a
Ben Li <ben_li5@yahoo.ca>
2017-03-02 00:54:35 +0000

    Adds looping exercises

---
+++ package-list.js
@@ -0,0 +1,6 @@
+var packagelist = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("kitchen stuff to pack:");
+for (var i=0; i < packagelist.length; i++) {
+  console.log(packagelist[i]);
+}
